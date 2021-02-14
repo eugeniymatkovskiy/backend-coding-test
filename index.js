7e9917a6f29fc8db85de3c7666464796ce004f7b
Ievgenii Matkivskyi <ievgenii.matkivskyi@connectwise.com>
2021-02-14 12:20:26 +0200

    added swagger

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const port = 8010;
 const sqlite3 = require('sqlite3').verbose();
+const expressSwaggerGen = require('express-swagger-generator');
 require('./winston');
 
 const db = new sqlite3.Database(':memory:');
 
 const buildSchemas = require('./src/schemas');
 const xenditApp = require('./src/app');
+const swaggerConf = require('./swagger');
 
 db.serialize(() => {
   buildSchemas(db);
 
   const app = xenditApp(db);
+  const expressSwagger = expressSwaggerGen(app);
+  // please use /api-docs for getting swagger explanation
+  expressSwagger(swaggerConf(port));
 
   app.listen(port, () => console.log(`App started and listening on port ${port}`));
 });
--- src/app.js
+++ src/app.js
@@ -13,8 +13,28 @@ const { defaultServerErr, ridesNotFoundErr } = require('./constants');
 
 module.exports = (db) => {
   const rideRepo = new RideRepository(db);
+
+  /**
+   * Check the health of server
+   * @route GET /health
+   * @returns {string} 200 - Status of the server
+   * @returns {Error}  default - Unexpected error
+   */
   app.get('/health', (req, res) => res.send('Healthy'));
 
+  /**
+   * Create new ride
+   * @route POST /rides
+   * @param {integer} start_lat.body.required - start latitude
+   * @param {integer} start_long.body.required - start long
+   * @param {integer} end_lat.body.required - end latitude
+   * @param {integer} end_long.body.required - end long
+   * @param {string} rider_name.body.required - rider name
+   * @param {string} driver_name.body.required - driver name
+   * @param {string} driver_vehicle.body.required - driver vehicle
+   * @returns {object} 200 - created ride
+   * @returns {Error}  default - Unexpected error
+   */
   app.post('/rides', jsonParser, validateNewRide, async (req, res) => {
     try {
       const rideData = prepareParamsForInsert(req.body);
@@ -29,6 +49,14 @@ module.exports = (db) => {
     }
   });
 
+  /**
+   * Get list of rides
+   * @route GET /rides
+   * @param {number} page.query - num of page
+   * @param {number} limit.query - amount of rides per page
+   * @returns {string} 200 - list of rides
+   * @returns {Error}  default - Unexpected error
+   */
   app.get('/rides', validateGetRides, async (req, res) => {
     try {
       const { limit, offset } = getDataForPagination(req.query);
@@ -45,6 +73,13 @@ module.exports = (db) => {
     }
   });
 
+  /**
+   * Get ride by id
+   * @route GET /rides/{id}
+   * @param {number} id.path.required - id of ride
+   * @returns {string} 200 - ride
+   * @returns {Error}  default - Unexpected error
+   */
   app.get('/rides/:id', validateGetRide, async (req, res) => {
     try {
       const { ride } = await rideRepo.getById(req.params.id);
